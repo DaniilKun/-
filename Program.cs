a3e92fa0c1c5b4a1d22de01157d1175583842d98
Даниил Куницын <daniilkun1997@mail.ru>
2022-03-08 23:42:47 +0400

    Source Control

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace klass_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя... ");
+            string name = Console.ReadLine();
+            
+                
+                Console.WriteLine("Введите возраст... ");
+                
+                double age = Convert.ToInt32(Console.ReadLine());
+                
+                Console.WriteLine("Введите название вашей профессии...");
+                string prof = Console.ReadLine();
+                
+                Console.WriteLine("Введите ваш рост(в метрах)...");
+                double height = Convert.ToDouble(Console.ReadLine());
+                
+                Console.WriteLine("расскажите о себе...");
+                string other = Console.ReadLine();
+
+                Console.WriteLine($"Имя:{name} Возраст:{age} Профессия:{prof} Рост:{height} О себе:{other}");
+                
+                
+                Console.ReadKey();
+        }
+    }
+}
